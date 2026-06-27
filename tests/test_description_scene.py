894048a5ca9b2d39c2b3a715bea44d20df21ddf4
timothijoe <zt1301112@gmail.com>
2026-06-27 23:13:08 +0800

    ok

--- tests/test_description_scene.py
+++ tests/test_description_scene.py
@@ -180,9 +180,11 @@ def test_right_tool_is_thin_knife_attached_below_force_sensor():
     assert model.geom_type[handle_id] == mujoco.mjtGeom.mjGEOM_BOX
     assert model.geom_type[blade_id] == mujoco.mjtGeom.mjGEOM_BOX
     assert model.geom_size[handle_id].tolist() == [0.012, 0.02, 0.04]
-    assert model.geom_size[blade_id].tolist() == [0.006, 0.04, 0.10]
+    assert model.geom_size[blade_id].tolist() == [0.006, 0.05, 0.10]
     # knife_blade extends further along body +Z than knife_handle
     assert model.geom_pos[blade_id, 2] > model.geom_pos[handle_id, 2]
+    # knife_handle is offset toward +Y (spine side), blade extends to -Y (edge side)
+    assert model.geom_pos[handle_id, 1] > model.geom_pos[blade_id, 1]
 
 
 def test_right_tool_uses_visual_box_with_simple_collision():
